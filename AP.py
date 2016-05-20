1e29f52fe8df86209db4e10641bb50d3f8f932c6
himanshu <himanshusharmahs974@gmail.com>
2016-05-20 08:44:24 +0530

    access point
    this program is used to grab the SSID and BSSID of the interface,oon mon0 interface.you can choose whichever is available  on your  machine.

---
+++ AP.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+import struct
+interface = 'wlan0'
+ap_list = []
+def info(fm):
+ if fm.haslayer(Dot11):
+  if ((fm.type == 0) & (fm.subtype==8)):
+   if fm.addr2 not in ap_list:
+    ap_list.append(fm.addr2)
+    print "SSID--> ",fm.info,"-- BSSID --> ",fm.addr2,
+    "-- Channel--> ", ord(fm[Dot11Elt:3].info)
+    sniff(iface=interface,prn=info)
+
